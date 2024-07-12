f92df119d01a5bdae5f471614e95f48d1b50356b
Jacqueline Díaz De León <70411766+jaquiddl@users.noreply.github.com>
2024-07-12 16:06:48 -0600

    Initial Commit

---
+++ Final-Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final-Project
+//
+//  Created by Jacqueline Diaz de Leon on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final-Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final-Project
+//
+//  Created by Jacqueline Diaz de Leon on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
